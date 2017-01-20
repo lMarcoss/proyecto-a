541779db75c5f6413298ced6683f2323b9248b48
leo <markos_193@hotmail.com>
2017-01-20 17:53:56 -0600

    modificando .gitignor

--- aserradero/src/java/dao/Conexion.java
+++ aserradero/src/java/dao/Conexion.java
@@ -9,29 +9,27 @@ import java.sql.SQLException;
  * @author lmarcoss
  */
 public class Conexion {
-    
+
     protected Connection conexion;
     //JDBC driver nombre y base de datos url
     private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     private final String DB_URL = "jdbc:mysql://localhost/aserradero";
-    
-    
+
     // base de datos credenciales
     private final String USER = "root";
     private final String PASSWORD = "root";
-//    private final String PASSWORD = "root";
-    
-    public void abrirConexion() throws Exception{
-        try{
+
+    public void abrirConexion() throws Exception {
+        try {
             Class.forName(JDBC_DRIVER);
-            conexion = DriverManager.getConnection(DB_URL,USER,PASSWORD);
-        }catch(SQLException e){
+            conexion = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (SQLException e) {
             throw e;
         }
     }
-    
-    public void cerrarConexion() throws SQLException{
-        if(conexion != null){
+
+    public void cerrarConexion() throws SQLException {
+        if (conexion != null) {
             conexion.close();
         }
     }
